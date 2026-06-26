a1fd0ee30f72114193126368ab3f5653f75ee6ac
ysabeldelacruz2001 <ysabeldelacruz2001@gmail.com>
2026-06-25 21:36:11 -0500

    Pide unca contraseña correcta

---
+++ pide_contraseña.py
@@ -0,0 +1,6 @@
+contraseña = ""
+while contraseña != "VETRINARIO":
+    contraseña = input("Escribe la contraseña: ").upper()
+    if contraseña != "VETRINARIO":
+     print("Contraseña incorrecta")
+print("¡Acceso concedido!")
